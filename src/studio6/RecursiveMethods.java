993ec0142ea5e3c1817f7ea27390c5a310fa25ea
Alex Lefkowitz <alefkowitz@wustl.edu>
2025-10-22 17:00:34 -0500

    I figured it out, you cant use the same array reordered, you have to make a new array

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -42,11 +42,15 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	/ */
-	public static int[] toReversed(int[] array) {
+	/*public static int[] toReversed(int[] array) {
 		int[] newArray = new int[array.length];
 			// FIXME create a helper method that can recursively reverse the given array
+			if (array.length>0){
 			return toReversedHelper(array, newArray, 0);
-		
+			}
+			else {
+				return newArray;
+			}
 	}
 	public static int[] toReversedHelper(int[] array, int[]newArray , int index){
 		newArray[index] = array[array.length-index-1];
@@ -56,10 +60,14 @@ public class RecursiveMethods {
 		else{
 		return toReversedHelper(array, newArray, index+1);
 		}
-	}
-	/*public static int[] toReversed(int[] array) {
+	}*/
+	public static int[] toReversed(int[] array) {
 		// FIXME create a helper method that can recursively reverse the given array
-		return toReversedHelper(array, (array.length/2)-1);
+		int[] newArray = new int[array.length];
+		for (int i =0; i< array.length; i++) {
+			newArray[i]=array[i];
+		}
+		return toReversedHelper(newArray, (array.length/2)-1);
 		
 	}
 	public static int[] toReversedHelper(int[] array, int index){
@@ -70,7 +78,7 @@ public class RecursiveMethods {
 		array[index] = array[array.length - index-1];
 		array[array.length - index-1] = temp;
 		return toReversedHelper(array, index-1);
-	}*/
+	}
 
 	/**
 	 * This method uses recursion to compute the greatest common divisor
